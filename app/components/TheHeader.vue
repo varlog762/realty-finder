8790631367d7f7ede560c7ced7a085d2687eb88b
Gregory Pereverzev <boot762@gmail.com>
2025-10-28 13:35:32 +0300

    feat: add TheHeader component

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>RealtyFinder</h1>
+    <nav>
+      <NuxtLink to="/">Dashboard</NuxtLink>
+      <NuxtLink to="/login">Login</NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <TheHeader />
+  <main>
+    <NuxtPage />
+  </main>
 </template>
 
 <style lang="scss" scoped></style>
